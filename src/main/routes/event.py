abc2aed26ac3219b069e14b4bce2a6686fe27775
Sabrina Gomes <sabrinagbdev@gmail.com>
2025-06-30 10:49:01 -0300

    feat: adiciona padronizacao dos dados HTTP

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,9 +1,16 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
 
 # agregador de rotas
 event_route_bp = Blueprint('event_route', __name__)
 
 @event_route_bp.route('/event', methods=['POST'])
 def create_event():
+    http_request = HttpRequest(body=request.json)
 
-    return jsonify({"message": "Evento criado com sucesso"}), 201
+    http_response = HttpResponse(body={"message": "Evento criado com sucesso"}, status_code=201)
+
+    return jsonify(http_response.body), http_response.status_code
+
